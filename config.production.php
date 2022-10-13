f93281494da556d62e8d7471f4dc678016356194
Roman Grinev <rigrinev@gmial.com>
2022-10-13 15:34:54 -0700

    Fix

--- config.production.php
+++ config.production.php
@@ -1,5 +1,16 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
     'production' => true,
+    'baseUrl' => '/',
+    'title' => 'Avansel Viewer',
+    'description' => 'Free Easy to use Opensource JavaScript Three.js based panorama / virtual tours viewer.',
+    'collections' => [
+        
+    ],
+    'selected' => function ($page, $section) {
+        return Str::contains($page->getPath(), $section) ? 'active' : '';
+    }
 ];
